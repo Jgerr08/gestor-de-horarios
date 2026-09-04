9ed8099f07d09dfd525661ca8ecaca4352394fd1
Jgerr08 <jesger2008@gmail.com>
2026-09-03 20:28:31 -0600

    Avance 2 terminado

--- gestor_de_horarios.py
+++ gestor_de_horarios.py
@@ -1,33 +1,52 @@
-#Declaración de arreglos donde guardaremos los datos necesarios para registrar y organizar las materias.
+# Declaración de arreglos
 materias = []
 dias = []
 horas_inicio = []
 horas_fin = []
 
-#Declaración de variable booleana que permite el ciclo while
+# Declaración de variable booleana que permite el ciclo while
 continuar = True
+contador = 0
+# Ciclo que se mantiene activo mientras el usuario siga añadiendo materias.
+while continuar:
 
-#Ciclo que se mantiene activo mientras el usuario siga añadiendo materias.
-while(continuar):
+    materia = input("Materia: ")
+    dia = input("Día: ")
+    hora_inicio = int(input("Hora inicio: "))
+    hora_fin = int(input("Hora fin: " ))
+    duracion = hora_fin - hora_inicio
+    empalme = False
 
-    materia = input("Materia:")
-    dia = input("Día:")
-    hora_inicio = int(input("Hora i:"))
-    hora_fin = int(input("Hora f:" ))
-    i=0
-    #Se recorre el arreglo de materias
+    if duracion <= 0:
+        print("La hora de fin debe ser mayor que la hora de inicio")
+        continue
+
+    # Se recorre el arreglo de materias
     for i in range(len(materias)):
-        #Se verifica que no haya empalme comprobando si el día es el mismo y si las horas chocan mediante operadores de comparación.
-        if(dia == dias[i] and hora_inicio < horas_fin[i] and hora_fin > horas_inicio[i]):
-            print("Existe un empalme en el horario")
-        i+=1
 
-    #Si no hay empalme se añaden los datos a los arreglos correspondientes.
-    materias.append(materia)
-    dias.append(dia)
-    horas_inicio.append(hora_inicio)
-    horas_fin.append(hora_fin)
+        # Se verifica que no haya empalme 
+        if (dia == dias[i] and hora_inicio < horas_fin[i]
+            and hora_fin > horas_inicio[i]):
+
+            print("Existe un empalme en el horario")
+            empalme = True
+            break
+            
+
+    # Si no hay empalme se añaden los datos a los arreglos correspondientes.
+    if not empalme: 
+        materias.append(materia)
+        dias.append(dia)
+        horas_inicio.append(hora_inicio)
+        horas_fin.append(hora_fin)
+        contador += 1
         
     respuesta = int(input("¿Desea agregar otra materia? (si = 1/no = 0)"))
-    continuar = respuesta == 1
-            
+    continuar = respuesta == 1 
+
+print("El número de materias inscritas es de:", contador)
+print("Detalles de tu horario:")
+
+for m in range(len(materias)):
+    print(f"Materia: {materias[m]}| Día: {dias[m]} \
+    | Hora de inicio: {horas_inicio[m]} | Hora de fin: {horas_fin[m]}")
